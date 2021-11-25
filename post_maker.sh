11d39cd2e442ff352c93025518ed5ebbb49624e6
mvrck96 <maverickmsk1@gmail.com>
2021-11-25 17:52:46 +0300

    fix for `hugo` date notation

--- post_maker.sh
+++ post_maker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TODAY=`date '+%Y.%m.%d'`
-TIMESTAMP=`date "+%Y%m%d_%H%M%S"`
+TODAY=$(date '+%Y-%m-%d')
+TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
 HEADER="---
 date: $TODAY
 title: \"$1\"
@@ -15,18 +15,16 @@ cover:
     caption: \"CAPTION HERE\"
 ---"
 
-if [[ $# -ne 0 ]]
-then
-  if [[ -d $1 ]]
-  then
+if [[ $# -ne 0 ]]; then
+  if [[ -d $1 ]]; then
     echo "Folder with this name already exists ! !"
     echo "Change name and re-run"
   else
     mkdir $1 && cd $1 && mkdir pics && touch index.md
-    echo -e "$HEADER" > index.md
+    echo -e "$HEADER" >index.md
   fi
 else
-  notitle="draft_post_$TIMESTAMP" 
+  notitle="draft_post_$TIMESTAMP"
   mkdir $notitle && cd $notitle && mkdir pics && touch index.md
-  echo -e "$HEADER" > index.md
+  echo -e "$HEADER" >index.md
 fi
